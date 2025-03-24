a9742a5bda1c94e15fc0b184c90368bac53ec8f9
isra <isranofrianti29@gmail.com>
2025-03-24 10:48:03 +0800

    phyton2

--- Materi Ke 3/1bit_sampai_24bit.py
+++ Materi Ke 3/1bit_sampai_24bit.py
@@ -1,2 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+image_1 = np.array([
+    [0,0,255,0,35,0,0],
+    [0,210,0,25,0,50,0],
+    [0,200,0,0,0,80,0],
+    [0,0,180,0,120,0,0],
+    [0,0,0,140,0,0,0],
+], dtype=np.uint8)
+
+image_2 = np.array([
+    [0,255,200],
+    [130,3,210],
+    [100,4,180]  
+], dtype=np.uint8)
+
