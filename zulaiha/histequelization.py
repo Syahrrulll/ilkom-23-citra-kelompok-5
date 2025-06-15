3e53afe3ba65482fc9450c4d0648cbe319aef441
Zulaiha19 <zulaiha.zha19@gmail.com>
2025-06-16 07:29:37 +0800

    baruuu

--- zulaiha/histequelization.py
+++ zulaiha/histequelization.py
@@ -1,3 +1,8 @@
 import cv2
 image = cv2.imread('sample.png', cv2.IMREAD_GRAYSCALE)
 equalized_image = cv2.equalizeHist(image)
+
+cv2.imshow("Original", image)
+cv2.imshow("Equalized", equalized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
