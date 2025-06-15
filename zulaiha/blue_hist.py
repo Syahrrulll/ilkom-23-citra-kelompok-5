40051fa10b8a092ee1c8f74dfb9631b7a27fe24e
Zulaiha19 <zulaiha.zha19@gmail.com>
2025-06-16 07:19:47 +0800

    baru

--- zulaiha/blue_hist.py
+++ zulaiha/blue_hist.py
@@ -2,3 +2,9 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+image = cv2.imread('sample.png')
+print(image.shape)
+cv2.imshow('all',image)
+cv2.imshow('blue',image[:,:,0])
+
+only_blue = image[:,:,0]
