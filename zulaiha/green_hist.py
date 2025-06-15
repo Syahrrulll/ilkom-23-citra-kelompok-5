4c43663f8a1b75756bc3931bc454032468375c97
Zulaiha19 <zulaiha.zha19@gmail.com>
2025-06-16 07:27:08 +0800

    baru

--- zulaiha/green_hist.py
+++ zulaiha/green_hist.py
@@ -2,3 +2,12 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+image = cv2.imread('sample.png')
+only_green = image[:,:,1]
+
+print(image.shape)
+
+cv2.imshow('all',image)
+cv2.imshow('green',only_green)
+
+histogram = cv2.calcHist([only_green], [0], None, [256], [0, 256])
