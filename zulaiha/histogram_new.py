786f14caa361bc15082bd08ea5a2b4dbb3cc66a4
Zulaiha19 <zulaiha.zha19@gmail.com>
2025-06-16 07:34:03 +0800

    neww

--- zulaiha/histogram_new.py
+++ zulaiha/histogram_new.py
@@ -11,4 +11,15 @@ def histogramEqual():
     cdf = hist.cumsum()
     cdfNorm = cdf * float(hist.max()) / cdf.max()
 
-    
+    plt.figure()
+    plt.subplot(231)
+    plt.imshow(img, cmap='gray')
+    plt.subplot(234)
+    plt.plot(hist)
+    plt.plot(cdfNorm, color='b')
+    plt.xlabel('pixel intesity')
+    plt.ylabel('# of pixels')
+    equImg = cv.equalizeHist(img)
+    equhist = cv.calcHist([equImg], [0], None, [256], [0,256])
+    equcdf = equhist.cumsum()
+    equcdfNorm = equcdf * float(equhist.max()) / equcdf.max()
