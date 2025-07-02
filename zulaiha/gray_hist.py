0a8363e5875c6ed551d9d19efb3a4a410b2449a5
isra <isranofrianti29@gmail.com>
2025-07-02 16:44:19 +0800

    update

--- zulaiha/gray_hist.py
+++ zulaiha/gray_hist.py
@@ -1,18 +1,33 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+import cv2  # Library OpenCV untuk pemrosesan gambar
+import numpy as np  # Library NumPy untuk manipulasi data numerik dan array
+import matplotlib.pyplot as plt  # Library Matplotlib untuk membuat grafik
 
+# Membaca gambar dalam mode grayscale (hitam-putih)
 image = cv2.imread('sample.png', cv2.IMREAD_GRAYSCALE)
+
+# Menampilkan dimensi gambar dalam format (tinggi, lebar)
 print(image.shape)
-cv2.imshow('gray',image)
 
+# Menampilkan gambar grayscale dengan jendela berjudul 'gray'
+cv2.imshow('gray', image)
 
+# Menghitung histogram dari gambar grayscale
+# [image]: gambar input
+# [0]: channel yang digunakan (0 untuk grayscale)
+# None: tidak menggunakan mask
+# [256]: jumlah bin (0–255)
+# [0, 256]: rentang nilai intensitas piksel
 histogram = cv2.calcHist([image], [0], None, [256], [0, 256])
 
+# Membuat figure untuk menampilkan grafik histogram
 plt.figure()
-plt.title("Histogram Grayscale")
-plt.xlabel("Intensitas Piksel")
-plt.ylabel("Frekuensi")
+plt.title("Histogram Grayscale")  # Judul grafik
+plt.xlabel("Intensitas Piksel")  # Label sumbu X (nilai 0-255)
+plt.ylabel("Frekuensi")  # Label sumbu Y (jumlah piksel untuk tiap intensitas)
+
+# Menampilkan histogram sebagai grafik garis
 plt.plot(histogram)
-plt.xlim([0, 256])
+plt.xlim([0, 256])  # Menentukan batas sumbu X dari 0 hingga 255
+
+# Menampilkan grafik histogram
 plt.show()
